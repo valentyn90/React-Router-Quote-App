0c6cfdc79dff319fe0558c416cbfe8da74c7f4fa
vladimir-bezrukov <web.dev903@gmail.com>
2021-05-21 17:16:33 +0200

    Added quote marks

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,10 +1,20 @@
 import { NavLink } from 'react-router-dom';
+import { ImQuotesLeft, ImQuotesRight } from 'react-icons/im';
 import classes from './MainNavigation.module.css';
 
 const MainNavigation = () => {
   return (
     <header className={classes.header}>
-      <div className={classes.logo}>Great Quotes</div>
+      <div className={classes.logo}>
+        <span className={classes.quote}>
+          <ImQuotesLeft />
+        </span>{' '}
+        Great Quotes{' '}
+        <span className={classes.quote}>
+          <ImQuotesRight />
+        </span>
+      </div>
+
       <nav className={classes.nav}>
         <ul>
           <li>
