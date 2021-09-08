f7bd28aeac88870d6e36c686761bb9c8ba4aec2c
vladimir-bezrukov <web.dev903@gmail.com>
2021-09-08 18:03:08 +0200

    Updates immer.js

--- node_modules/immer/src/core/immerClass.ts
+++ node_modules/immer/src/core/immerClass.ts
@@ -42,8 +42,6 @@ export class Immer implements ProducersFns {
 			this.setUseProxies(config!.useProxies)
 		if (typeof config?.autoFreeze === "boolean")
 			this.setAutoFreeze(config!.autoFreeze)
-		this.produce = this.produce.bind(this)
-		this.produceWithPatches = this.produceWithPatches.bind(this)
 	}
 
 	/**
@@ -65,7 +63,7 @@ export class Immer implements ProducersFns {
 	 * @param {Function} patchListener - optional function that will be called with all the patches produced here
 	 * @returns {any} a new state, or the initial state if nothing was modified
 	 */
-	produce(base: any, recipe?: any, patchListener?: any) {
+	produce: IProduce = (base: any, recipe?: any, patchListener?: any) => {
 		// curried invocation
 		if (typeof base === "function" && typeof recipe !== "function") {
 			const defaultBase = recipe
@@ -123,7 +121,11 @@ export class Immer implements ProducersFns {
 		} else die(21, base)
 	}
 
-	produceWithPatches(arg1: any, arg2?: any, arg3?: any): any {
+	produceWithPatches: IProduceWithPatches = (
+		arg1: any,
+		arg2?: any,
+		arg3?: any
+	): any => {
 		if (typeof arg1 === "function") {
 			return (state: any, ...args: any[]) =>
 				this.produceWithPatches(state, (draft: any) => arg1(draft, ...args))
@@ -183,7 +185,7 @@ export class Immer implements ProducersFns {
 		this.useProxies_ = value
 	}
 
-	applyPatches(base: Objectish, patches: Patch[]) {
+	applyPatches<T extends Objectish>(base: T, patches: Patch[]): T {
 		// If a patch replaces the entire state, take that replacement as base
 		// before applying patches
 		let i: number
@@ -198,12 +200,12 @@ export class Immer implements ProducersFns {
 		const applyPatchesImpl = getPlugin("Patches").applyPatches_
 		if (isDraft(base)) {
 			// N.B: never hits if some patch a replacement, patches are never drafts
-			return applyPatchesImpl(base, patches)
+			return applyPatchesImpl(base, patches) as any
 		}
 		// Otherwise, produce a copy of the base state.
 		return this.produce(base, (draft: Drafted) =>
 			applyPatchesImpl(draft, patches.slice(i + 1))
-		)
+		) as any
 	}
 }
 
